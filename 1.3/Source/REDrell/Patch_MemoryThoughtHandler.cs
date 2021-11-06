9107dfa3541826d3daa714ff6904a0929dd3d520
neronix17 <39186138+neronix17@users.noreply.github.com>
2021-11-06 01:52:06 +0000

    Apparel Implemented

---
+++ 1.3/Source/REDrell/Patch_MemoryThoughtHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+using HarmonyLib;
+
+namespace REDrell
+{
+    [HarmonyPatch(typeof(MemoryThoughtHandler), "TryGainMemory", new Type[] { typeof(Thought_Memory), typeof(Pawn) })]
+    public static class Patch_MemoryThoughtHandler
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Thought_Memory newThought, Pawn otherPawn)
+        {
+            if(newThought.def == ThoughtDefOf.GotSomeLovin && otherPawn?.def == REDrellDefOf.RE_Drell)
+            {
+                Thought_Memory drellLovin = (Thought_Memory)ThoughtMaker.MakeThought(REDrellDefOf.RE_DrellLovin);
+                otherPawn.needs.mood.thoughts.memories.TryGainMemory(drellLovin, otherPawn);
+            }
+        }
+    }
+}
---
+++ 1.3/Source/REDrell/REDrellDefOf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace REDrell
+{
+    [DefOf]
+    public class REDrellDefOf
+    {
+        public REDrellDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(REDrellDefOf));
+        }
+
+        public static ThingDef RE_Drell;
+        public static ThoughtDef RE_DrellLovin;
+    }
+}
